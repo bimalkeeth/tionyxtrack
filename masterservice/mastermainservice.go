8398236757f49706254ea32ae1d185daefcc2508
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2020-07-12 16:58:18 +1000

    add masterservice proto for api service

--- masterservice/mastermainservice.go
+++ masterservice/mastermainservice.go
@@ -3,10 +3,9 @@ package main
 import (
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/util/log"
-	 masterpoto "tionyxtrack/masterservice/proto"
-	 api "tionyxtrack/masterservice/protoapi"
+	masterpoto "tionyxtrack/masterservice/proto"
+	api "tionyxtrack/masterservice/protoapi"
 )
-
 
 func main() {
 	service := micro.NewService(
@@ -14,8 +13,8 @@ func main() {
 	)
 	service.Init()
 
-	err:=masterpoto.RegisterMasterServiceHandler(service.Server(), new(api.MasterService))
-	if err !=nil{
+	err := masterpoto.RegisterMasterServiceHandler(service.Server(), new(api.MasterService))
+	if err != nil {
 		log.Log(err)
 	}
 	if err := service.Run(); err != nil {
