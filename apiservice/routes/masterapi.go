8c9298ad067c874c95c7a3134807b8f8d18c1749
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2020-07-12 17:25:37 +1000

    add selector for client

--- apiservice/routes/masterapi.go
+++ apiservice/routes/masterapi.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/selector/roundrobin"
 	mast "tionyxtrack/apiservice/services/master"
 )
 
 var masterService micro.Service
 
 func init() {
-	masterService = micro.NewService(micro.Name("go.micro.srv.master"))
+	masterService = micro.NewService(micro.Name("go.micro.srv.master"), micro.Selector(roundrobin.NewSelector()))
 	masterService.Init()
 
 }
