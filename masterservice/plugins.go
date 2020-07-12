ae71e1a7b431b9535958624336120654e7daa2fc
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2020-07-12 20:01:21 +1000

    go plugins imported

--- masterservice/plugins.go
+++ masterservice/plugins.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/micro/go-plugins/registry/etcd/v2"
+	_ "github.com/micro/go-plugins/registry/etcdv3/v2"
 	_ "github.com/micro/go-plugins/registry/kubernetes/v2"
 	_ "github.com/micro/go-plugins/transport/nats/v2"
 )
