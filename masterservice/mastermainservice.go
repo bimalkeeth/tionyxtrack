939c3582e0b513d69ce3691bc2c35091f0cefd51
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2020-07-12 21:59:42 +1000

    go plugins imported

--- masterservice/mastermainservice.go
+++ masterservice/mastermainservice.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/util/log"
+	_ "github.com/micro/go-plugins/transport/nats/v2"
 	masterpoto "tionyxtrack/masterservice/proto"
 	api "tionyxtrack/masterservice/protoapi"
 )
 
 func main() {
+
 	service := micro.NewService(
 		micro.Name("go.micro.srv.master"),
 	)
